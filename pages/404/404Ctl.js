f6b20ac79a529beb8805f9e344d77392e4e076b2
Mark <mark@marktai.com>
2016-07-03 21:36:16 +0000

    chatting and file linting

--- pages/404/404Ctl.js
+++ pages/404/404Ctl.js
@@ -1,10 +1,10 @@
 marktai.controller("404Ctl", ["$scope", "$rootScope", "$resource", "$location", function($scope, $rootScope, $resource, $location) {
-  $rootScope.info = ""
-  $rootScope.error = ""
+    $rootScope.info = ""
+    $rootScope.error = ""
 
-  $scope.badPath = $location.hash()
-  $scope.badAbs = $location.absUrl().replace($location.url(), $scope.badPath)
-  $rootScope.page = "404";
+    $scope.badPath = $location.hash()
+    $scope.badAbs = $location.absUrl().replace($location.url(), $scope.badPath)
+    $rootScope.page = "404";
 
-  // $rootScope.CheckCredentials();
+    // $rootScope.CheckCredentials();
 }])
